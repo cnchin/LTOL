600b9b41020fa261693eed7237efc941e56b4ff9
chin <2283351621@qq.com>
2022-03-05 00:37:13 +0800

    test ui

--- main.js
+++ main.js
@@ -13,6 +13,11 @@ ui.layout(
             </horizontal>
         </vertical>
 
+        <horizontal gravity="center" marginTop="24">
+            <text id="progress_value" textColor="black" textSize="16sp" margin="8" text="0"/>
+            <progressbar id="progress" w="*" style="@style/Base.Widget.AppCompat.ProgressBar.Horizontal"/>
+        </horizontal>
+
         <horizontal gravity="center_horizontal" marginTop="600">
             <button id="start" text="开始" textSize="20" textColor="#fffef9" textStyle="bold" bg="#51c4d3" marginLeft="20"/>
             <button id="stop" text="暂停" textSize="20" textColor="#fffef9" textStyle="bold" bg="#51c4d3" marginLeft="20"/>
@@ -37,3 +42,49 @@ ui.cb_ito.on("check",(checked)=>{
         toast("取消了ITO测试");
     }
 });
+
+
+var testId = null;
+
+
+ui.start.click(() => {
+    if (testId != null) {
+        stopTest();
+    } else {
+        startTest();
+    }
+});
+
+ui.stop.click(() => {
+    if (testId != null) {
+        stopTest();
+    } else {
+        startTest();
+    }
+});
+
+ui.cancel.click(() => {
+    stopTest();
+});
+
+function startTest() {
+    if (ui.progress.getProgress() == 10) {
+        ui.progress.setProgress(0);
+    }
+    toast("开始测试");
+    startId = setInterval(() => {
+        var p = ui.progress.getProgress();
+        p++;
+        if (p > 10) {
+            overTest();
+        }
+        ui.progress.setProgress(p);
+        ui.progress_value.setText(p.toString());
+    }, 200);
+}
+
+function overTest() {
+    toast("结束测试");
+    clearInterval(testId);
+    testId = null;
+}
