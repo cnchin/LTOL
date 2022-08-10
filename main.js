9ec51b10a6ca59a11036aaed5fbf303e4aa8c706
chin <57850145+cnchin@users.noreply.github.com>
2022-08-10 22:32:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,20 +5,15 @@ ui.layout(
         <vertical>
             <horizontal gravity="center" marginTop="100">
                 <checkbox id="cb_reboot" text="    设置重启测试次数" textColor="#22a2c3" textSize="25" textStyle="bold"></checkbox>
-                <input id="reboot_fre" inputType="number" marginLeft="20"/>
+                <input id="cb_reboot_fre" inputType="number" marginLeft="20"/>
             </horizontal>
             <horizontal gravity="center">
                 <checkbox id="cb_ito" text="    设置容值测试次数" textColor="#22a2c3" textSize="25" textStyle="bold"></checkbox>
-                <input id="reboot_fre" inputType="number" marginLeft="20"/>
+                <input id="cb_ito_fre" inputType="number" marginLeft="20"/>
             </horizontal>
         </vertical>
 
-        <horizontal gravity="center" marginTop="24">
-            <text id="progress_value" textColor="black" textSize="16sp" margin="8" text="0"/>
-            <progressbar id="progress" w="*" style="@style/Base.Widget.AppCompat.ProgressBar.Horizontal"/>
-        </horizontal>
-
-        <horizontal gravity="center_horizontal" marginTop="600">
+        <horizontal gravity="center_horizontal" marginTop="300">
             <button id="start" text="开始" textSize="20" textColor="#fffef9" textStyle="bold" bg="#51c4d3" marginLeft="20"/>
             <button id="stop" text="暂停" textSize="20" textColor="#fffef9" textStyle="bold" bg="#51c4d3" marginLeft="20"/>
             <button id="cancel" text="取消" textSize="20" textColor="#fffef9" textStyle="bold" bg="#51c4d3" marginLeft="20"/>
@@ -26,65 +21,67 @@ ui.layout(
     </vertical>
 )
 
+//test item button
+var rb_checked=false
+var ito_checked=false
+
+//test item num
+var rb_num=0
+var ito_num=0
 
 ui.cb_reboot.on("check",(checked)=>{
     if(checked){
-        toast("勾选了重启测试");
+        //toast("勾选了重启测试");
+        rb_checked=true
     }else{
-        toast("取消了重启测试");
+        //toast("取消了重启测试");
+        rb_checked=false
     }
 });
 
 ui.cb_ito.on("check",(checked)=>{
     if(checked){
-        toast("勾选了ITO测试");
+        //toast("勾选了ITO测试");
+        ito_checked=true
     }else{
-        toast("取消了ITO测试");
+        //toast("取消了ITO测试");
+        ito_checked=false
     }
 });
 
+var check_item_verify =function(){
+    var n=0;
+    if(rb_checked){
+        if(ui.cb_reboot_fre.getText()<=0){
+            log("重启未设定次数");
+            n=-1;
+        }
+        else{
+            n++;
+        }
+    }
 
-var testId = null;
+    if(ito_checked){
+        if(ui.cb_ito_fre.getText()<=0){
+            log("容值未设定次数");
+            n=-1;
+        }else{
+            n++;
+        }
+    }
 
+    if(n > 0){
+        log("启动测试",",一共 ",n," 项测试");
+        toast("启动测试"+",一共 "+ n +" 项测试");
+    }else if(n ==0){
+        toast("未勾选测试！")
+    }else{
+        log("参数有误，放弃测试");
+        toast("参数有误，放弃测试");
+    }
+}
+ 
 
 ui.start.click(() => {
-    if (testId != null) {
-        stopTest();
-    } else {
-        startTest();
-    }
+    check_item_verify()
 });
-
-ui.stop.click(() => {
-    if (testId != null) {
-        stopTest();
-    } else {
-        startTest();
-    }
-});
-
-ui.cancel.click(() => {
-    stopTest();
-});
-
-function startTest() {
-    if (ui.progress.getProgress() == 10) {
-        ui.progress.setProgress(0);
-    }
-    toast("开始测试");
-    startId = setInterval(() => {
-        var p = ui.progress.getProgress();
-        p++;
-        if (p > 10) {
-            overTest();
-        }
-        ui.progress.setProgress(p);
-        ui.progress_value.setText(p.toString());
-    }, 200);
-}
-
-function overTest() {
-    toast("结束测试");
-    clearInterval(testId);
-    testId = null;
-}
